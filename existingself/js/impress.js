1147e9c6b1a0de7c5cb6b224408973de3e81d614
balasan <byslava@gmail.com>
2015-02-08 22:17:38 -0600

    fix

--- existingself/js/impress.js
+++ existingself/js/impress.js
@@ -163,7 +163,7 @@
         var props = {
             position: "absolute",
             transformOrigin: "top left",
-            transition: "transform 0s ease-in-out",
+            transition: "all 0s ease-in-out",
             transformStyle: "preserve-3d"
         }
         
@@ -187,10 +187,7 @@
             return !!(el && el.id && stepData["impress-" + el.id]);
         }
         
-        // steps.forEach(function ( el, idx ) {
-
-
-        var doEl =function(el,idx){    
+        steps.forEach(function ( el, idx ) {
             var data = el.dataset,
                 step = {
                     translate: {
@@ -222,22 +219,7 @@
                 transformStyle: "preserve-3d"
             });
             
-        };
-
-
-        var i = 0;                     //  set your counter to 1
-
-        function myLoop () {           //  create a loop function
-           setTimeout(function () {    //  call a 3s setTimeout when the loop is called
-              doEl(steps[i],i)
-              i++;                     //  increment the counter
-              if (i < steps.length) {            //  if the counter < 10, call the loop function
-                 myLoop();             //  ..  again which will trigger another 
-              }                        //  ..  setTimeout()
-           }, 1)
-        }
-
-        myLoop();                      //  start the loop
+        });
 
         // making given step active
 
